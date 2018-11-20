f4317d7be2421dd4b3b6b584eec9d9ae6d816576
FooBar167 <foobar167@gmail.com>
2018-11-20 23:26:16 +0300

    Hello WebGL (not finished)

--- 01_hello_weblg/hello_webgl.js
+++ 01_hello_weblg/hello_webgl.js
@@ -4,6 +4,10 @@ const canvas = document.getElementById('webgl');
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 
+var gl = null;  // WebGL rendering context
+var extensions = null;  // array of supported extensions
+window.onload = InitializeWebGL;  // init WebGL when DOM is ready
+
 // Get WebGL context, if standard is not available; fall back on alternatives
 function GetWebGLContext(canvas) {
     return canvas.getContext('webgl') ||               // standard
@@ -15,23 +19,26 @@ function GetWebGLContext(canvas) {
 // and some other browser implementations.
 }
 
-var gl = null;  // WebGL rendering context
-var extensions = null;  // array of supported extensions
+function InitializeWebGL() {
+    // ! used twice in a row to cast object state to a Boolean value
+    if (!!window.WebGLRenderingContext === true) {
+        if (gl = GetWebGLContext(canvas)) {  // initialize WebGL rendering context, if available
+            console.log('WebGL is initialized.');
 
-// ! used twice in a row to cast object state to a Boolean value
-if (!!window.WebGLRenderingContext === true) {
-    if (gl = GetWebGLContext(canvas)) {  // initialize WebGL rendering context, if available
-        console.log('WebGL is initialized.');
+            // Ensure OpenGL viewport is resized to match canvas dimensions
+            gl.viewportWidth = canvas.width;
+            gl.viewportHeight = canvas.height;
 
-        // Ensure OpenGL viewport is resized to match canvas dimensions
-        gl.viewportWidth = canvas.width;
-        gl.viewportHeight = canvas.height;
+            console.log(gl);  // output the WebGL rendering context object to console for reference
+            console.log(extensions = gl.getSupportedExtensions());  // list available extensions
 
-        console.log(gl);  // output the WebGL rendering context object to console for reference
-        console.log(extensions = gl.getSupportedExtensions());  // list available extensions
-
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);  // set screen clear color to black RGBA
-        gl.clear(gl.COLOR_BUFFER_BIT);  // enable color; required for clearing the screen
-        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);  // clear viewport with black
-    } else { console.log('Your browser does not support WebGL'); }
-} else { console.log('WebGL is supported, but disabled :-('); }
+            gl.clearColor(0.0, 0.0, 0.0, 1.0);  // set screen clear color to black RGBA
+            gl.clear(gl.COLOR_BUFFER_BIT);  // enable color; required for clearing the screen
+            //gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);  // clear viewport with black
+        } else {
+            console.log('Your browser does not support WebGL.');
+        }
+    } else {
+        console.log('WebGL is supported, but disabled :-(');
+    }
+}
