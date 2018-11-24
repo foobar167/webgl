8700e94bf6350c013fdfbd8392be23e12b435a37
FooBar167 <foobar167@gmail.com>
2018-11-25 01:54:40 +0300

    Hello WebGL (not finished)

--- 01_hello_weblg/hello_webgl.js
+++ 01_hello_weblg/hello_webgl.js
@@ -1,44 +1,189 @@
 const canvas = document.getElementById('webgl');
 
-//Set canvas width and height
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
+let gl = null;  // WebGL rendering context
 
-var gl = null;  // WebGL rendering context
-var extensions = null;  // array of supported extensions
-window.onload = InitializeWebGL;  // init WebGL when DOM is ready
+window.onload = initializeWebGL;  // init WebGL when DOM is ready
+window.addEventListener('resize', drawScene);  // redraw when the window is resized
 
-// Get WebGL context, if standard is not available; fall back on alternatives
-function GetWebGLContext(canvas) {
-    return canvas.getContext('webgl') ||               // standard
-           canvas.getContext('experimental-webgl') ||  // alternative, Safari, etc.
-           canvas.getContext('moz-webgl') ||           // Firefox, Mozilla
-           canvas.getContext('webkit-3d');             // last try. Safari, maybe others
-// Note that 'webgl' is not available as of Safari version <= 7.0.3
-// So we have to fall back to ambiguous alternatives for it,
-// and some other browser implementations.
+function initializeWebGL() {
+    if (!window.WebGLRenderingContext) {
+        console.log('WebGL is supported, but disabled :-(');
+        return;
+    }
+
+    gl = getWebGLContext(canvas);  // initialize WebGL rendering context, if available
+    if (!gl) {
+        console.log('Your browser does not support WebGL.');
+        return;
+    }
+
+    // Vertex shader program
+    const vertexSource = `
+        attribute vec4 aVertexPosition;
+    
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
+    
+        void main() {
+          gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        }
+    `;
+
+    // Fragment shader program
+    const fragmentSource = `
+        void main() {
+          gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);  // yellow color
+        }
+    `;
+
+    // Initialize a shader program; this is where all the lighting is established.
+    const shaderProgram = initShaderProgram(vertexSource, fragmentSource);
+
+    // Collect all the info needed to use the shader program. Look up which attribute
+    // shader program is using for aVertexPosition and look up uniform locations.
+    gl._programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+        },
+    };
+
+    // Call the routine that builds all the drawing objects.
+    gl._buffers = initBuffers();
+
+    drawScene();  // draw the scene
+
+    console.log('WebGL is initialized.');
+    console.log(gl);  // output the WebGL rendering context object to console for reference
+    console.log(gl.getSupportedExtensions());  // print list of supported extensions
 }
 
-function InitializeWebGL() {
-    // ! used twice in a row to cast object state to a Boolean value
-    if (!!window.WebGLRenderingContext === true) {
-        if (gl = GetWebGLContext(canvas)) {  // initialize WebGL rendering context, if available
-            console.log('WebGL is initialized.');
+// Get WebGL context, if standard is not available, then try on different alternatives
+function getWebGLContext(canvas) {
+    return canvas.getContext('webgl') ||               // standard
+           canvas.getContext('experimental-webgl') ||  // Safari, etc.
+           canvas.getContext('moz-webgl') ||           // Firefox, Mozilla
+           canvas.getContext('webkit-3d');             // last try, Safari and maybe others
+}
 
-            // Ensure OpenGL viewport is resized to match canvas dimensions
-            gl.viewportWidth = canvas.width;
-            gl.viewportHeight = canvas.height;
+// Initialize a shader program, so WebGL knows how to draw the data.
+function initShaderProgram(vertexSource, fragmentSource) {
+    const vertexShader = loadShader(gl.VERTEX_SHADER, vertexSource);
+    const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fragmentSource);
 
-            console.log(gl);  // output the WebGL rendering context object to console for reference
-            console.log(extensions = gl.getSupportedExtensions());  // list available extensions
+    // Create the shader program
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
 
-            gl.clearColor(0.0, 0.0, 0.0, 1.0);  // set screen clear color to black RGBA
-            gl.clear(gl.COLOR_BUFFER_BIT);  // enable color; required for clearing the screen
-            //gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);  // clear viewport with black
-        } else {
-            console.log('Your browser does not support WebGL.');
-        }
-    } else {
-        console.log('WebGL is supported, but disabled :-(');
+    // If creating the shader program failed, alert
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+    return shaderProgram;
+}
+
+// Create shader of the given type, upload the source and compile it.
+function loadShader(type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);  // send the source to the shader object
+    gl.compileShader(shader);  // compile the shader program
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  // check if it compiled successfully
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
+
+// Initialize the buffers of a simple two-dimensional square.
+function initBuffers() {
+    const positionBuffer = gl.createBuffer();  // create a buffer for the square's positions
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);  // select the buffer
+    // Create an array of positions for the square.
+    const positions = [ 1.0,  1.0,
+                       -1.0,  1.0,
+                        1.0, -1.0,
+                       -1.0, -1.0];
+    // Pass the list of positions into WebGL to build the shape by creating a Float32Array
+    // from the JavaScript array, then use it to fill the current buffer.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    return { position: positionBuffer };
+}
+
+// Draw the scene.
+function drawScene() {
+    canvas.width  = window.innerWidth;  // set canvas width and height
+    canvas.height = window.innerHeight;
+    gl.viewportWidth = canvas.width;    // ensure OpenGL viewport is resized to match canvas
+    gl.viewportHeight = canvas.height;
+    gl.clearColor(0.2, 0.2, 0.2, 1.0);  // set screen clear color to gray, fully opaque
+    gl.clearDepth(1.0);                 // clear everything
+    gl.enable(gl.DEPTH_TEST);           // enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // near things obscure far things
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  // clear the canvas
+
+    const fieldOfView = 45 * Math.PI / 180;   // FOV in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;  // see objects between 0.1 units and 100 units away from the camera
+    const zFar = 100.0;
+    const projectionMatrix = mat4.create();
+
+    // glmatrix.js always has the first argument as the destination to receive the result.
+    mat4.perspective(projectionMatrix,
+                     fieldOfView,
+                     aspect,
+                     zNear,
+                     zFar);
+
+    // Set the drawing position to the "identity" point, which is the center of the scene.
+    const modelViewMatrix = mat4.create();
+
+    // Move the drawing position a bit
+    mat4.translate(modelViewMatrix,     // destination matrix
+                   modelViewMatrix,     // matrix to translate
+                   [-0.0, 0.0, -6.0]);  // amount to translate
+
+    // Tell WebGL how to pull out the positions from the position
+    // buffer into the vertexPosition attribute.
+    {
+        const numComponents = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, gl._buffers.position);
+        gl.vertexAttribPointer(
+            gl._programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(gl._programInfo.attribLocations.vertexPosition);
+    }
+
+    gl.useProgram(gl._programInfo.program);  // tell WebGL to use our program when drawing
+
+    // Set the shader uniforms
+    gl.uniformMatrix4fv(
+        gl._programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix);
+    gl.uniformMatrix4fv(
+        gl._programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix);
+
+    {
+        const offset = 0;
+        const vertexCount = 4;
+        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
     }
 }
